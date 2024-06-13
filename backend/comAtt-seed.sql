6bcc21eded28309e52fbfb00a4c54bee61785a6f
Erkan Turker <erkanturker@Erkans-MacBook-Pro.local>
2024-06-13 18:30:11 -0400

    update seed file

---
+++ backend/comAtt-seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username,password,first_name,last_name,email,role)
+VALUES ('admin',
+        '$2b$12$deSniSUBq.G6IeMp1PJptODGKu/ARp/V.hBwQ1MAjGFq2JRXtPKa6',
+        'admin',
+        'test',
+        'admin@gmail.com',
+        'admin');
--- backend/comAtt.sql
+++ backend/comAtt.sql
@@ -1,14 +1,22 @@
-\echo 'Delete and recreate comAtt db?' \prompt 'Return for yes or control-c to cancel > ' user_confirmation \echo 'Dropping and recreating the comAtt database...'
+\echo 'Delete and recreate comAtt db?'
+\prompt 'Return for yes or control-c to cancel > ' user_confirmation 
+\echo 'Dropping and recreating the comAtt database...'
+
 DROP DATABASE IF EXISTS comatt;
-
-
 CREATE DATABASE comatt;
 
 -- Connect to the newly created database
-\connect comatt \i comAtt-schema.sql \echo 'Delete and Recreate comAtt db' \prompt 'Return for yes or control-c to cancel >' user_input \echo 'Dropping and recreating the comAtt database...'
+\connect comatt
+\i comAtt-schema.sql
+
+\i comAtt-seed.sql
+
+\echo 'Delete and Recreate comAtt db'
+\prompt 'Return for yes or control-c to cancel >' user_input 
+\echo 'Dropping and recreating the comAtt database...'
+
 DROP DATABASE IF EXISTS comatt_test;
-
-
 CREATE DATABASE comatt_test;
 
-\connect comatt_test \i comtAtt-schema.sql
+\connect comatt_test 
+\i comtAtt-schema.sql
